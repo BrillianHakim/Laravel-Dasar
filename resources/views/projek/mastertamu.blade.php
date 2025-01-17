c0b7d73728f37189b17742eeaf021bd77c1c5699
BrillianHakim <bintangbrillianhakim@gmail.com>
2025-01-17 21:27:17 +0700

    projek sederhana

--- resources/views/projek/mastertamu.blade.php
+++ resources/views/projek/mastertamu.blade.php
@@ -1,11 +1,21 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Buku Tamu</title>
+  <link rel="stylesheet" href="{{ asset('assets/BukuTamu.css') }}">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="{{ asset('assets/style.css') }}">
 </head>
 <body>
+  <div class="sidebar">
+    <a href="#">Dashboard</a>
+    <a href="/bukutamu">Buku Tamu</a>
+  </div>
+
+  <div class="content">
     @yield('konten')
+  </div>
 </body>
 </html>
